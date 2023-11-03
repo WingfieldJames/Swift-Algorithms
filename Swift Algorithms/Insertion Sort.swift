eaa18f9e170bdc1b1ac1fea6ab0ae700d1d2356b
James Wingfield <wingfield.j@etoncollege.org.uk>
2023-11-03 16:55:55 +0000

    Started coding Insertion Sort

---
+++ Swift Algorithms/Insertion Sort.swift
@@ -0,0 +1,19 @@
+//
+//  Insertion Sort.swift
+//  Swift Algorithms
+//
+//  Created by Wingfield, James (PEPW) on 02/11/2023.
+//
+
+import Foundation
+
+
+
+var unSorted = [5,6,3,7,2,9]
+var temp = 0
+
+for element in unSorted{
+    if element != unSorted[0]{
+        if element <
+    }
+}
--- Swift Algorithms/main.swift
+++ Swift Algorithms/main.swift
@@ -7,5 +7,20 @@
 
 import Foundation
 
-print("Hello, World!")
+var unSorted = [5,6,3,7,2,9]
+var temp = 0
+
+for element in unSorted{
+    if element != unSorted[0]{
+        if element < unSorted[element-1]{c}
+            temp = element
+            element = unSorted[element-1]
+            unSorted[element-1] = temp
+    }
+}
+
+
+
+
+
 
